f2865fa24b39e2467f3751fe9633d6a1cbac1764
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2024-01-08 09:19:01 +0100

    Exercici 1.2

---
+++ src/iniciar-sessio-fradera/iniciar-sessio-fradera.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciar-sessio-fradera',
+  templateUrl: './iniciar-sessio-fradera.component.html',
+  styleUrls: ['./iniciar-sessio-fradera.component.css']
+})
+
+export class IniciarSessioFraderaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
